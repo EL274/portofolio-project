65d16f084d7f3e5376ce3a5a5987a63069edecf8
tresor@foxkiradev <tresormakosso510@gmail.com>
2025-02-05 15:52:34 +0100

    initialisation du fichier server

---
+++ FinEase/back-end/models/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require ('cors');
+require('dotenv').config(); 
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI, { userNewUrlParser: true, })
