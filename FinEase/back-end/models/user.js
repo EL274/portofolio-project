7f09919bf9f940e2c9050a39eeba91c0910d7aec
tresor@foxkiradev <tresormakosso510@gmail.com>
2025-02-10 03:01:40 +0100

    ok

---
+++ FinEase/back-end/models/user.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const UserSchema = new mongoose.Schema({
+    nom: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    motDepasse: { type: String, required: true }
+});
+
+UserSchema.pre('save', async function (next) {
+    if (this.isModified('motDePasse')) {
+        this.motDePasse = await bcrypt.hash(this.motDePasse, 10);
+    }
+    next();
+});
+
+module.exports = mongoose.model('User', UserSchema);
