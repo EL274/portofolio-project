42013ed80d2f2c7f5ea94224db2f418543114903
tresor@foxkiradev <tresormakosso510@gmail.com>
2025-02-11 02:35:42 +0100

    ok

---
+++ FinEase/back-end/routes/users.js
@@ -0,0 +1,38 @@
+const express =  require ('express');
+const User = require('../models/User');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+const router = express.Router();
+
+//put your ID(inscription)
+router.post('/inscription', async (req, res) => {
+  const { nom, email, motDepasse } = req.body;
+  try {
+    const user = new User({ nom, email, motDepasse});
+    await user.save();
+    res.status(201).json({ message: 'User créé' });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+//connexion 
+router.post('/connexion', async (req, res) => {
+  const { email, motDepasse } = req.body;
+  try {
+    const user = await User.findOne({ email });
+    if (user && await bcrypt.compare(motDepasse, user.motDePasse)) {
+      const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+        expiresIn: '1h'
+      });
+      res.json({ token });
+    } else {
+      res.status(401).json({ message: 'Email ou mot de passe incorrect' });
+    }
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+module.exports = router;
+
