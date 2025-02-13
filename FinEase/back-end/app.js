53a773bb44aec6d0ec3810c5473ee5aacb2cb821
tresor@foxkiradev <tresormakosso510@gmail.com>
2025-02-13 16:36:00 +0100

    initialisation des routes

--- FinEase/back-end/app.js
+++ FinEase/back-end/app.js
@@ -11,4 +11,11 @@ app.use(cors());
 app.use(express.json());
 
 // Connexion à MongoD
-mongoose.connect(process.env.MONGO_URI, { userNewUrlParser: true })
+mongoose.connect(process.env.MONGO_URI, { userNewUrlParser: true, useUnifieldTopology: true })
+ .then(() => console.log('connecté à MongoDB'))
+ .catch((err) => console.error('Erreur de connexion à MongoDB:', err));
+
+ //Routes
+ app.use('/api/depense', require('./routes/depenses'))
+ app.use('/api/objectifs', require('./routes/objectifs'))
+ app.use('/api/users', require('./routes/users'))
