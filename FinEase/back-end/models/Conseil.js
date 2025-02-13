3dbadff78af0199f4c80e84e596240ff60fe682b
tresor@foxkiradev <tresormakosso510@gmail.com>
2025-02-13 12:15:12 +0100

    ok

--- FinEase/back-end/models/Conseil.js
+++ FinEase/back-end/models/Conseil.js
@@ -3,10 +3,10 @@ const mongoose = require('mongoose');
 
 const ConseilSchema = new mongoose.Schema({
     titre: { type: String, required: true },
-    description: { type: String, rquired: true },
+    description: { type: String, required: true },
     type: { type: String, enum: ['conseil', 'ressource', 'outil'], required: true }, // Type de solution
     lien: { type: String }, // Lien vers une ressource externe
 });
 
-modules.exports = mongoose.model('Conseil', ConseilSchema)
+module.exports = mongoose.model('Conseil', ConseilSchema)
  
