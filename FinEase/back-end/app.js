7e56f780b7205a99a4f442b11a819e67bc943517
tresor@foxkiradev <tresormakosso510@gmail.com>
2025-02-13 14:49:56 +0100

    ok

--- FinEase/back-end/app.js
+++ FinEase/back-end/app.js
@@ -10,18 +10,5 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// Connexion à MongoDB
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('Connecté à MongoDB'))
-  .catch(err => console.error('Erreur de connexion à MongoDB:', err));
-
-// Routes
-app.get('/', (req, res) => {
-  res.send('API de gestion de budget');
-});
-
-// Démarrer le serveur
-app.listen(PORT, () => {
-  console.log(`Serveur en écoute sur le port ${PORT}`);
-});
-
+// Connexion à MongoD
+mongoose.connect(process.env.MONGO_URI, { userNewUrlParser: true })
