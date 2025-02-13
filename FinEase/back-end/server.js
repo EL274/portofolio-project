2824c958598ffdcc4f701096ec52bbb39d367634
tresor@foxkiradev <tresormakosso510@gmail.com>
2025-02-13 13:55:07 +0100

    initialisation du serveur avec app.js

--- FinEase/back-end/server.js
+++ FinEase/back-end/server.js
@@ -1,26 +1,7 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-require('dotenv').config();
-
-const app = express();
-const PORT = process.env.PORT || 5000;
-
-// Middleware
-app.use(cors());
-app.use(express.json());
-
-// Connexion à MongoDB
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('Connecté à MongoDB'))
-  .catch(err => console.error('Erreur de connexion à MongoDB:', err));
-
-// Routes
-app.get('/', (req, res) => {
-  res.send('API de gestion de budget');
-});
+const app = express()
+const PORT = process.env.PORT || 5000
 
 // Démarrer le serveur
-app.listen(PORT, () => {
-  console.log(`Serveur en écoute sur le port ${PORT}`);
+app.listen (PORT, () => {
+  console.log(`serveur en écoute sur le port ${PORT}`);
 });
