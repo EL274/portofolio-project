761baf53c4abb1a3e7996ff04174bbb5f1633f97
tresor@foxkiradev <tresormakosso510@gmail.com>
2025-02-19 12:09:57 +0100

    ok

---
+++ FinEase/back-end/middleware/auth.js
@@ -0,0 +1,13 @@
+const jwt = require('jsonwebtoken');
+exports.verifierAuth = (req, res, next) => {
+    const token = req.header('Authorization')?.replace('Bearer ','');
+    if (!token) return res.status(401).json({ message: 'Accès refusé'});
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.userId = decoded.userId;
+        next();
+    } catch (err) {
+      res.status(401).json({ message: 'Token invalide' });  
+    }
+};
