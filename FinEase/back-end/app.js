c0f480c654a13ccbe7672533965b4de081cf0975
tresor@foxkiradev <tresormakosso510@gmail.com>
2025-02-19 12:33:27 +0100

    ok

--- FinEase/back-end/app.js
+++ FinEase/back-end/app.js
@@ -16,8 +16,10 @@ mongoose.connect(process.env.MONGO_URI, { userNewUrlParser: true, useUnifieldTop
  .catch((err) => console.error('Erreur de connexion à MongoDB:', err));
 
  //Routes
- app.use('/api/depense', require('./routes/depenses'))
- app.use('/api/objectifs', require('./routes/objectifs'))
- app.use('/api/users', require('./routes/users'))
- app.use('/api/categories')
+ app.use('/api/depense', require('./routes/depenses'));
+ app.use('/api/objectifs', require('./routes/objectifs'));
+ app.use('/api/users', require('./routes/users'));
+ app.use('/api/categories', require('./routes/catégories'));
+ app.use('/api/conseils', require('./routes/conseils'));
+ 
  module.exports = app;
