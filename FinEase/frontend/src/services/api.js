1bb64e6cb7e192e1cfb184fca5752482a67ae779
tresor@foxkiradev <tresormakosso510@gmail.com>
2025-02-25 09:00:31 +0100

    delete and add api

--- FinEase/frontend/src/services/api.js
+++ FinEase/frontend/src/services/api.js
@@ -1,1 +1,13 @@
+import axios from 'axios';
+import { response } from '../../../back-end/app';
+const API_URL = 'http://localhost:5000/api'; 
 
+export const getDepenses = async () => {
+    const response = await axios.get(`${API_URL}/depenses`);
+    return response.data;
+};
+
+export const createDepense = async (depenseData) => {
+    const reponse = await axios.post(`${API_URL}/depenses`, depenseData);
+    return response.data;
+};
