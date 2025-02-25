83ef39249343d66682fe7cf8db0869da991370f6
tresor@foxkiradev <tresormakosso510@gmail.com>
2025-02-25 14:57:52 +0100

    ok

---
+++ FinEase/frontend/src/pages/HomePage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HomePage = () => {
+    return (
+        <div>
+            <h1>Bienvenue sur FinEase</h1>
+            <p>Une application revolutionnaire pour une gestion plus de votre budget</p>
+        </div>
+    );
+};
+
+export default HomePage;
