3fe03ccd5d973ede5dac7c1adc1d5b60dfef6234
tresor@foxkiradev <tresormakosso510@gmail.com>
2025-02-26 15:58:11 +0100

    ok

--- FinEase/frontend/src/App.js
+++ FinEase/frontend/src/App.js
@@ -1,14 +1,30 @@
 import React from 'react';
 import DepenseList from './components/DepenseList';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import DepensesPage from './pages/DepensesPage';
+import UsersPage from './pages/UsersPage';
+import CategoriesPage from './pages/Categories'; 
+import ConseilsPage from './pages/Conseils';
+import ObjectifsPage from './pages/Objectifs';
+import login from './pages/Auth/loginPage';
+import RegisterPage from './pages/Auth/RegisterPage';
 
 function App() {
   return (
-    <div className="App">
-      <h1></h1>
-    </div>
-  )
+    <Router>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/depenses" element={<DepensesPage />} />
+            <Route path="/users" element={<UsersPage />} />
+            <Route path="/categories" element={<CategoriesPage />} />
+            <Route path="/conseils" element={<ConseilsPage />} />
+            <Route path="/objectifs" element={<ObjectifsPage/>} />
+            <Route path="/login" element={<loginPage />} />
+            <Route path="/register" element={<RegisterPage />} />
+          </Routes>
+    </Router>
+  );
 }
-
 
 export default App;
