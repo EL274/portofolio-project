b896f05c2c0d8f3e20c0e57ef188f0254fa40b4a
tresor@foxkiradev <tresormakosso510@gmail.com>
2025-02-26 03:00:06 +0100

    ok

---
+++ FinEase/frontend/src/pages/Auth/RegisterPage.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import { registrer } from '../services/api';
+
+const RegisterPage = () => {
+    const [nom, setNom] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const user = await registrer({ nom, email, password });
+        if (user) {
+            //Rediriger vers la page de connexion ou le tableau de bord
+        }
+    };
+    return (
+        <div>
+            <h2>Inscription</h2>
+            <form onSubmit={handleSubmit}>
+                <input
+                type="text"
+                placeholder="Non"
+                value={non}
+                onChange={(e) => setNom(e.target.value)}
+                />
+                <input
+                 type="email"
+                 placeholder="Email"
+                 value={email}
+                 onChange={(e) => setEmail(e.target.value)}
+                />
+                <input
+                 type="password"
+                 placeholder="Mot de passe"
+                 value={password}
+                 onChange={(e) => setPassword(e.target.value)}
+                />
+                <button type="submit">S'inscrire</button>
+            </form>
+        </div>
+    );
+};
+ 
+export default RegisterPage;
