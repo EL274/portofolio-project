490445d5e632cbbc22f539cdab76dac06d219369
tresor@foxkiradev <tresormakosso510@gmail.com>
2025-03-02 00:41:25 +0100

    modified

--- FinEase/frontend/src/pages/Auth/LoginPage.js
+++ FinEase/frontend/src/pages/Auth/LoginPage.js
@@ -1,37 +1,56 @@
 import React, { useState } from 'react';
-import { login } from '../services/api';
+import { useNavigate } from 'react-router-dom';
+import { login } from '../../services/api';
 
 const LoginPage = () => {
     const [email, setEmail] = userState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
+    const navigate = useNavigate();
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-        const user = await login({ email, passeword });
+        setError('');
+
+        try {
+
+        const user = await login({ email, password });
         if (user) {
             // Rediriger vers la page d'accueil ou le tableau de bord
+            navigate('/'); //Redirige vers la page d'accuei après la connexion
         }
-    };
-    
+    } catch (err) {
+        setError('Identifiants incorrects. Veuillez réessayer.');
+    }
+};
     return (
         <div>
             <h2>Connexion</h2>
-            <form onSubmit={handleSubmit}>
+            {error && <p style={{ color: 'red' }}>{error}</p>}
+            <form onSubmit={handleSubmit}> 
+                <div>
+                <label>Email :</label>
                 <input
                  type="email"
                  placeholder="Email"
                  value={email}
                  onChange={(e) => setEmail(e.target.value)}
+                 required 
                 />
+                </div>
+                <div>
+                  <label>Mot de passe :</label>  
                 <input
                  type="password"
                  placeholder="Mot de passe"
                  value={password}
                  onChange={(e) => setPassword(e.target.value)}
+                 required
                 />
+                </div>
                 <button type="submit">Se connecter</button>
             </form>
         </div>
-
     );
 };
 
