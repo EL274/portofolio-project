79f31f45a1759265326dee3f5cbf07c013500f6d
tresor@foxkiradev <tresormakosso510@gmail.com>
2025-03-02 01:47:13 +0100

    ok

--- FinEase/frontend/src/App.js
+++ FinEase/frontend/src/App.js
@@ -7,7 +7,7 @@ import UsersPage from './pages/UsersPage';
 import CategoriesPage from './pages/Categories'; 
 import ConseilsPage from './pages/Conseils';
 import ObjectifsPage from './pages/Objectifs';
-import LoginPage from './pages/login';
+import LoginPage from './pages/Auth/LoginPage';
 import RegisterPage from './pages/Auth/RegisterPage';
 
 function App() {
@@ -20,7 +20,7 @@ function App() {
             <Route path="/categories" element={<CategoriesPage />} />
             <Route path="/conseils" element={<ConseilsPage />} />
             <Route path="/objectifs" element={<ObjectifsPage/>} />
-            <Route path="/login" element={<loginPage />} />
+            <Route path="/login" element={<LoginPage />} />
             <Route path="/register" element={<RegisterPage />} />
           </Routes>
     </Router>
