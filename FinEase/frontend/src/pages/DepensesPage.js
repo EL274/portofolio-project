17b4c7e92388c41294778a90532e40468989a0cf
tresor@foxkiradev <tresormakosso510@gmail.com>
2025-03-02 02:58:29 +0100

    ok

--- FinEase/frontend/src/pages/DepensesPage.js
+++ FinEase/frontend/src/pages/DepensesPage.js
@@ -2,22 +2,25 @@ import React, { useEffect, useState } from 'react';
 import { getDepenses } from '../services/api';
 import DepenseList from '../components/DepenseList';
 
-const DepenseList = () => {
+const DepensesPage = () => {
     const [depenses, setDepenses] = useState([]);
 
     useEffect(() => {
         const fetchDepenses = async () => {
+            try {
             const data = await getDepenses();
             setDepenses(data);
-        };
+        } catch (error) {
+            console.error("Erreur lors de la récupération des dépenses :", error);
+        }
+    };
         fectchDepenses();
     }, []);
     
     return (
         <div>
             <h2>Gestion des dépenses</h2>
-            <DepenseList depenses={depenses}/>
-        </div>
+            <DepenseList depenses={depenses} />       </div>
     );
 };
 
