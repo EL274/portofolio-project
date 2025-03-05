37a9f9ef5a6b99204dfe5e4930be1e8fa3ab8c7a
tresor@foxkiradev <tresormakosso510@gmail.com>
2025-03-05 23:47:48 +0100

    ok

--- FinEase/back-end/server.js
+++ FinEase/back-end/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 
-
+const Categories = require('./routes/Categories')
 
 const app = express();
 const PORT = process.env.PORT || 5000;
