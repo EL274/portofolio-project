a49e7c85a23463cd134b5e11b06760d9af667ef9
tresor@foxkiradev <tresormakosso510@gmail.com>
2025-03-06 09:39:18 +0100

    modification

--- FinEase/back-end/routes/conseils.js
+++ FinEase/back-end/routes/conseils.js
@@ -1,9 +1,17 @@
 const express = require('express');
- const { genererConseils } = require('../service/conseilService');
-const express = express.router();
+const { genererConseils } = require('../service/conseilService');
+const router  = express.Router();
 
-router.get ('/:user_id ', async (req, res) => {
+router.get ('/:user_id', async (req, res) => {
     try {
+        const userId = req.params.user_Id;
+
+        if (!userId) {
+            return res.status(400).json({ message: "L' ID user est requis."});
+        }
+        
+        console.log(`generer des conseils pour le user : ${userId}`);
+
         const conseils = await genererConseils(req.params.user_id);
         res.json(conseils);
     } catch (err) {
