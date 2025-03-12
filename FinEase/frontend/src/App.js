42498436f2d38a3b4e2bd5c2557ad920f1df0b1e
tresor@foxkiradev <tresormakosso510@gmail.com>
2025-03-12 10:29:36 +0100

    ol

--- FinEase/frontend/src/App.js
+++ FinEase/frontend/src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Dashboard from './pages/Dashboard';
+import AddDepense from './pages/AddDepense';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <Router>
+    <Navbar />
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/add-depense" element={<AddDepense />} />
+      <Route path="/" element={<Login />} />
+    </Routes>
+    <Footer />
+  </Router>
+);
 
 export default App;
