257d863943fe20996bdf54fe4342fc3abe2da78d
tresor@foxkiradev <tresormakosso510@gmail.com>
2025-03-18 13:36:11 +0100

    modification

--- FinEase-app-web/frontend/src/pages/Home.js
+++ FinEase-app-web/frontend/src/pages/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 import BackgroundVideo from '../components/BackgroundVideo';
+import videoSrc from '../asset/background-video';
 
 const HomeContainer = styled.div`
   display: flex;
