d5ba96c05d1a6de1015e4917c12c4bbd469f488c
tresor@foxkiradev <tresormakosso510@gmail.com>
2025-03-25 15:14:00 +0100

    modification

--- FinEase-app-web/backend/db.js
+++ FinEase-app-web/backend/db.js
@@ -3,13 +3,8 @@ const mongoose = require('mongoose');
 // Fonction pour se connecter à MongoDB
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-    });
+        await mongoose.connect(process.env.MONGO_URI);
         console.log(" Connecté à MongoDB");
-    
-    console.log('Connecté à MongoDB');
     } catch (error) {
         console.error(" Erreur de connexion à MongoDB :", error);
         process.exit(1); // Arrête le serveur si la connexion échoue
