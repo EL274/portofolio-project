c8d11bc87feaf968bc98402ba40aeba8f4c8a935
tresor@foxkiradev <tresormakosso510@gmail.com>
2025-05-30 16:41:05 +0200

    ok

--- FinEase-app-web/backend/test-debug.js
+++
@@ -1,9 +0,0 @@
-const bcrypt = require('bcryptjs');
-
-(async () => {
-  const hashFromDB = '$2b$10$ZORR6YIHlAfXMpDTcKo9H.b/BbXL47r4Z2HfNXkGMxzK2YKl3OLqS'
-  const plainPassword = "TRes8R@.5"; // Mot de passe que tu essaies
-
-  const isMatch = await bcrypt.compare(plainPassword, hashFromDB);
-  console.log("Résultat :", isMatch ? "✅ CORRECT" : "❌ FAUX");
-})();
